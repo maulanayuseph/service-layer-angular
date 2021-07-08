ad1aca0ce5bed4956985bf3f9f5485ff544294fa
Maulana Yuseph <maulanayuseph@gmail.com>
2021-07-08 11:43:58 +0700

    move function on app-component to services and load again on app-component

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,13 +1,24 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { 
+  constructor(private http:HttpClient) { 
+  }
 
-    console.log("creating CoursesService");
+  loadCourses() : Observable<Course[]>{
+    
+    const params = new HttpParams()
+          .set("page",1)
+          .set("pageSize", "10");
+
+    return this.http.get<Course[]>('/api/courses', {params});
 
   }
 }
